76349d626599ada49c2be2dc4aed133f3b5ab9c9
Haraprasad <haraprasadsatapathy@gmail.com>
2016-08-10 20:00:50 +0530

    test

--- app/src/main/java/com/suyogcomputech/helper/Constants.java
+++ app/src/main/java/com/suyogcomputech/helper/Constants.java
@@ -14,6 +14,7 @@ import java.util.Date;
  */
 public final class Constants {
     public static final String COLOR_WHITE = "#FFFFFF";
+    public static final String TEST = "#FFFFFF";
     public static final String Request = "request";
     public static final String RESPONSE = "response";
 
